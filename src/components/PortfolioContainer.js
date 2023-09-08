035b28972a86a0159a44c6f7ff9f63b6278cee0e
Nephew20 <cofieldk20@gmail.com>
2023-09-08 15:29:49 -0400

    fixed navigation and header styles

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Header from './Header'
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Resume from './pages/Resume';
@@ -7,12 +6,14 @@ import Contact from './pages/Contact'
 import NavBar from './Navbar';
 import Footer from './Footer';
 
+
+
 function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    
+
     if (currentPage === 'About') {
       return <About />;
     } else if (currentPage === 'Portfolio') {
@@ -27,10 +28,8 @@ function PortfolioContainer() {
 
   return (
     <div>
-      <Header />
-      {/* We are passing the currentPage from state and the function to update it */}
-      <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
-      {/* Here we are calling the renderPage method which will return a component  */}
+        <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+        {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
 
       <Footer />
