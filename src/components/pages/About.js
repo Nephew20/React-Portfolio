4f84faae23dc8525726d28f875359800613a0424
Nephew20 <cofieldk20@gmail.com>
2023-09-11 16:16:56 -0400

    added css for the about section and portfolio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,27 @@
 import React from "react";
 import headshot from '../../images/headshot.JPG'
+import '../../styles/about.css'
 
 function About() {
+
+    // const styles = {
+    //   marginStyle: {
+    //     margin-right: '20px'
+    //   }
+    // }
     return (
-        <div>
+        <div className="about-section">
             <h2> About Page </h2>
-            <p> Hello! My name is Kevin Cofield and I am a Biomedical Engineer turned Software Developer. I Currently live in Atlanta, GA and my skills are primarily in backend development. </p>
-            <img src={headshot} alt="pic-of-me" />
+
+            <div className="content-container">
+                <div className="headshot">
+                    <img src={headshot} alt="pic-of-me" />
+                </div>
+                
+                <div className="about-info">
+                    <p> Hello! My name is Kevin Cofield and I am a Biomedical Engineer turned Software Developer. I Currently live in Atlanta, GA and my skills are primarily in backend development. </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,41 +5,46 @@ import StudyGuide from '../../images/first-application.JPG'
 import Scheduler from '../../images/Work Scheduler Screenshot.JPG'
 import NoteTaker from '../../images/NoteTaker.jpg'
 import WeatherDashboard from '../../images/Weather Dashboard.jpg'
+import '../../styles/portfolio.css'
 
 function Portfolio() {
     return (
-        <div>
-            <h3> Side Hustle </h3>
-            <a href="https://side-hustle.herokuapp.com/">
-                <img src={SideHustle} alt="Side Hustle Project" />
-            </a>
+        <div className="port-section">
+            <h2>Portfolio</h2>
 
-            <h3> Travel Dashboard </h3>
-            <a href="https://kritishrestha.github.io/Traveler-Dashboard/" >
-                <img src={TravelDashboard} alt="Dashboard" />
-            </a>
-            
-            <h3> Study Guide </h3>
-            <a href="https://nephew20.github.io/prework-study-guide/" >
-                <img src={StudyGuide} alt="Study Guide" />
-            </a>
-            
-            <h3> Scheduler </h3>
-            <a href="https://nephew20.github.io/Scheduler/" >
-                <img src={Scheduler} alt="Scheduler" />
-            </a>
+            <div className="port-images">
+                
+                <h3> Side Hustle </h3>
+                <a href="https://side-hustle.herokuapp.com/">
+                    <img src={SideHustle} alt="Side Hustle Project" style={{width: '30%'}}/>
+                </a>
 
-            <h3> Note Taker </h3>
-            <a href="https://mighty-temple-98807.herokuapp.com/" >
-                <img src={NoteTaker} alt="Note Taker" />
-            </a>
+                <h3> Travel Dashboard </h3>
+                <a href="https://kritishrestha.github.io/Traveler-Dashboard/" >
+                    <img src={TravelDashboard} alt="Dashboard" />
+                </a>
 
-            <h3> Weather Dashboard </h3>
-            <a href=" https://nephew20.github.io/Weather-Dashboard/" >
-                <img src={WeatherDashboard} alt="Weather Dashboard" />
-            </a>
+                <h3> Study Guide </h3>
+                <a href="https://nephew20.github.io/prework-study-guide/" >
+                    <img src={StudyGuide} alt="Study Guide" />
+                </a>
 
-           
+                <h3> Scheduler </h3>
+                <a href="https://nephew20.github.io/Scheduler/" >
+                    <img src={Scheduler} alt="Scheduler" />
+                </a>
+
+                <h3> Note Taker </h3>
+                <a href="https://mighty-temple-98807.herokuapp.com/" >
+                    <img src={NoteTaker} alt="Note Taker" />
+                </a>
+
+                <h3> Weather Dashboard </h3>
+                <a href=" https://nephew20.github.io/Weather-Dashboard/" >
+                    <img src={WeatherDashboard} alt="Weather Dashboard" />
+                </a>
+            </div>
+
         </div>
     )
 }
